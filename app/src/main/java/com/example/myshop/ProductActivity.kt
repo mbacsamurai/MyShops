c19f32c8179e61360be2e4af0b3979d6e39eeca4
sakda lekdee <l.sakda@nishikawa.local>
2019-08-27 08:58:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myshop/ProductActivity.kt
@@ -0,0 +1,44 @@
+package com.example.myshop
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_product.*
+
+
+class ProductActivity : AppCompatActivity() {
+
+     private var parentLinearLayout : LinearLayout ?= null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+         parentLinearLayout = findViewById(R.id.parent_linear_layout)
+
+
+
+    }
+   fun onAddField(v: View) {
+       val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+       val rowView = inflater.inflate(R.layout.add_field, null)
+       parentLinearLayout?.addView(rowView, parentLinearLayout?.childCount!! - 1)
+   }
+    fun onDeleteField(v:View){
+        parentLinearLayout?.removeView((v as LinearLayout).focusedChild)
+    }
+
+
+}
+
